179f3bdadb75a72c47f8793556d57562d8e278b8
Marcia <marcia.pires@ua.pt>
2022-06-20 16:40:02 +0100

    Latest revised version

--- text/models/tf-idf.py
+++ text/models/tf-idf.py
@@ -1,15 +1,13 @@
-import math
 from collections import Counter
 
 import pandas as pd
 import numpy as np
 from imblearn.under_sampling import RandomUnderSampler
-from sklearn.naive_bayes import MultinomialNB
 
 from sklearn.utils import shuffle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from sklearn.metrics import f1_score, recall_score, make_scorer, mean_absolute_error, mean_squared_error
+from sklearn.metrics import f1_score, recall_score, make_scorer
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -52,21 +50,20 @@ def train_test(X, y, test="../test_split_Depression_AVEC2017.csv"):
 
 
 X_train, X_test, y_train, y_test = train_test(X_dense, y)
-X_train = X_train.reshape((142, 79698))
-X_test = X_test.reshape((45, 79698))
+X_train = X_train.reshape((142, 78765))
+X_test = X_test.reshape((45, 78765))
 
 y_train = y_train.reshape(142)
 y_test = y_test.reshape(45)
 
 X_train, y_train = shuffle(X_train, y_train, random_state=42)
-print(X_train.shape)
-print(y_train.shape)
+
+
 # undersampling
 
 undersample = RandomUnderSampler(sampling_strategy='majority')
 X_train, y_train = undersample.fit_resample(X_train, y_train)
 #print(Counter(y_train))
-X_train, y_train = shuffle(X_train, y_train, random_state=42)
 
 
 def k_cross_validation(model, grid, X=X_train, y=y_train):
@@ -76,8 +73,6 @@ def k_cross_validation(model, grid, X=X_train, y=y_train):
 
     f1_score_res = []
     recall_res = []
-    mae_res = []
-    rmse_res = []
 
     for train_index, test_index in rkf.split(X):
         X_train, X_test = X[train_index], X[test_index]
@@ -92,31 +87,20 @@ def k_cross_validation(model, grid, X=X_train, y=y_train):
         recall = recall_score(y_test, predict_values)
         recall_res.append(recall)
 
-        mae = mean_absolute_error(y_test, predict_values)
-        mae_res.append(mae)
-
-        mse = mean_squared_error(y_test, predict_values)
-        rmse = math.sqrt(mse)
-        rmse_res.append(rmse)
-
-    return f1_score_res, recall_res, mae_res, rmse_res, cv.best_params_
+    return f1_score_res, recall_res, cv.best_params_
 
 
 def logistic_regression():
 
     grid = {"C": np.logspace(-3, 3, 7), "penalty": ["l1", "l2"]}
-    #grid = [{'C': np.logspace(-3, 3, 7), 'penalty': ['l1'], 'solver': ['liblinear', 'sag', 'saga']},
-    #        {'C': np.logspace(-3, 3, 7), 'penalty': ['l2'], 'solver': ['lbfgs', 'newton-cg']}]
     #model = LogisticRegression(n_jobs=3, C=10000)
     model = LogisticRegression(solver='liblinear')
-    f1_score_res, recall_res, mae_res, rmse_res, best_params = k_cross_validation(model, grid)
+    f1_score_res, recall_res, best_params = k_cross_validation(model, grid)
 
     f1_val = np.mean(f1_score_res)
     recall_val = np.mean(recall_res)
-    mae_val = np.mean(mae_res)
-    rmse_val = np.mean(rmse_res)
 
-    print(f"Logistic Regression\nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\nMAE: {mae_val}\nRMSE: {rmse_val}\n")
+    print(f"Logistic Regression\nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\n")
     print("Tuned hpyerparameters :", best_params)
 
 
@@ -127,19 +111,17 @@ def random_forest():
 
     grid = {
         'n_estimators': [1, 10],
-        'max_features': ['auto', 'sqrt', 'log2'],
-        'max_depth': [4, 5, 6, 7, 8],
-        'criterion': ['gini', 'entropy']
+        #'max_features': ['auto', 'sqrt', 'log2'],
+        #'max_depth': [4, 5, 6, 7, 8],
+        #'criterion': ['gini', 'entropy']
     }
     model = RandomForestClassifier(random_state=42)
-    f1_score_res, recall_res, mae_res, rmse_res, best_params = k_cross_validation(model, grid)
+    f1_score_res, recall_res, best_params = k_cross_validation(model, grid)
 
     f1_val = np.mean(f1_score_res)
     recall_val = np.mean(recall_res)
-    mae_val = np.mean(mae_res)
-    rmse_val = np.mean(rmse_res)
 
-    print(f"Random Forest\nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\nMAE: {mae_val}\nRMSE: {rmse_val}\n")
+    print(f"Random Forest\nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\n")
     print("Tuned hpyerparameters :", best_params)
 
 
@@ -151,18 +133,16 @@ def decision_tree():
         'criterion': ['gini', 'entropy'], 'max_depth': [2, 4, 6, 8, 10, 12],
     }
     model = DecisionTreeClassifier(random_state=42, min_samples_leaf=10)
-    f1_score_res, recall_res, mae_res, rmse_res, best_params = k_cross_validation(model, grid)
+    f1_score_res, recall_res, best_params = k_cross_validation(model, grid)
 
     f1_val = np.mean(f1_score_res)
     recall_val = np.mean(recall_res)
-    mae_val = np.mean(mae_res)
-    rmse_val = np.mean(rmse_res)
 
-    print(f"Decision Tree\nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\nMAE: {mae_val}\nRMSE: {rmse_val}\n")
+    print(f"Decision Tree\nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\n")
     print("Tuned hyperparameters :", best_params)
 
 
-decision_tree()
+#decision_tree()
 
 
 def svm():
@@ -171,32 +151,13 @@ def svm():
                         {'kernel': ['poly'], 'degree': [3, 4, 5], 'C': [1, 10, 100]},
                         {'kernel': ['linear'], 'C': [1, 10, 100]}]
     model = SVC()
-    f1_score_res, recall_res, mae_res, rmse_res, best_params = k_cross_validation(model, grid)
-
-    f1_val = np.mean(f1_score_res)
-    recall_val = np.mean(recall_res)
-    mae_val = np.mean(mae_res)
-    rmse_val = np.mean(rmse_res)
-
-    print(f"SVM \nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\nMAE: {mae_val}\nRMSE: {rmse_val}\n")
-    print("Tuned hyperparameters :", best_params)
-
-
-#svm()
-
-
-def multinomialNB():
-    grid = {}
-    model = MultinomialNB()
-
     f1_score_res, recall_res, best_params = k_cross_validation(model, grid)
 
     f1_val = np.mean(f1_score_res)
     recall_val = np.mean(recall_res)
 
-    print(f"Multinomial Naive Bayes \nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\n")
+    print(f"SVM \nBest F1 Score: {f1_val}\nBest Recall Score: {recall_val}\n")
     print("Tuned hyperparameters :", best_params)
 
 
-#multinomialNB()
-
+svm()
